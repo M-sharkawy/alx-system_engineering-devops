bacea89e8e8e448529d8e42222a6857078c26edd
M-sharkawy <mostafasharks021@gmail.com>
2025-02-26 00:34:56 +0200

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''fetch first 10 hot posts title of subreddit'''
+
+import requests
+
+
+def top_ten(subreddit):
+    """Return the first 10 hot posts title of subreddit"""
+    limit = 10
+    posts = []
+    url = "https://www.reddit.com/r/{}/{}.json?Limit={}".format(subreddit,"top",limit)
+    headers = {
+        "Users-Agent": "linux:0x16.api.advanced:v1.0.0"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        if not data.get('data').get('children'):
+            return None
+        for post in data['data']['children']:
+            x = post['data']['title']
+            posts.append(x)
+        return posts
+    return 0
