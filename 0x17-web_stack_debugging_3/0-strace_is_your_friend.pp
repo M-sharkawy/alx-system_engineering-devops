629908f6ae4c8fe5099e12d4042b22dae5cb8333
M-sharkawy <mostafaeelsharkawy@gmail.com>
2025-02-27 11:30:31 +0200

    task 0 edits

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 # Fixes bad `phpp` extensions in causing 500 error code
-
 exec { 'fix-wordpress-server-errors':
   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
